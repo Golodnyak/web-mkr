c95e2b47edd28a432bd4a5372073741881b715fa
Roma <GolodnyakRoma@gmail.com>
2022-11-10 10:10:22 +0200

    done mkr(var1)

---
+++ app.js
@@ -0,0 +1,17 @@
+const next = document.getElementById('next');
+const back = document.getElementById('prev');
+const reset = document.getElementById('reset');
+const display = document.getElementById('display');
+const number =1;
+next.onclick=()=>{
+   const prev =display.innerText;
+    display.innerText=Number(prev)+1
+};
+
+back.onclick=()=>{
+   const prev =display.innerText;
+    display.innerText=Number(prev)-1
+};
+reset.onclick=()=>{
+    display.innerText=Number(0);
+};
